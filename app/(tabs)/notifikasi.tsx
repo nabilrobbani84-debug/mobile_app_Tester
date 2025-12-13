59504b7bb26b079ab5723a7d36fe52eb0d2c7669
nabilrobbani84 <nabilrobbani6@gmail.com>
2025-12-13 08:54:42 +0700

    Update notifikasi.tsx

--- app/(tabs)/notifikasi.tsx
+++ app/(tabs)/notifikasi.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import ProfileScreen, { NotificationsScreen } from '../../src/views/screens/notifications.screen'; // Pastikan path ini sesuai
+import NotificationsScreen from '../../src/views/screens/notifications.screen'; // Pastikan path ini sesuai
 
-export default function ProfilPage() {
-  return <NotificationsScreen  />;
+export default function NotificationsPage() {
+  return <NotificationsScreen />
 }
