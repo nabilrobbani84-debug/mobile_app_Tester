b0b973c050371d483488a9198c6467990ab7c82b
nabilrobbani84 <nabilrobbani6@gmail.com>
2025-12-14 23:26:35 +0700

    Update login.tsx

--- app/login.tsx
+++ app/login.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import { Stack } from 'expo-router';
 // @ts-ignore: Mengabaikan check tipe untuk file JS legacy
 import LoginScreen from '../src/views/screens/LoginScreen'; 
 
 export default function LoginRoute() {
-  return <LoginScreen />;
+  return (
+    <>
+      {/* Mengonfigurasi opsi layar khusus untuk login: sembunyikan header bawaan */}
+      <Stack.Screen options={{ headerShown: false }} />
+      
+      <LoginScreen />
+    </>
+  );
 }
