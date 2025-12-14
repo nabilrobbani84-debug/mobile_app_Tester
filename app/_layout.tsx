5313bda2a72c22580540321b60da67ed663af3ee
nabilrobbani84 <nabilrobbani6@gmail.com>
2025-12-14 23:26:29 +0700

    Update _layout.tsx

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,6 +4,8 @@ import { StatusBar } from 'expo-status-bar';
 import 'react-native-reanimated';
 
 import { useColorScheme } from '@/hooks/use-color-scheme';
+// Import AuthProvider dari state management Anda
+import { AuthProvider } from '../src/state/AuthContext';
 
 export const unstable_settings = {
   anchor: '(tabs)',
@@ -14,10 +16,16 @@ export default function RootLayout() {
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
-      </Stack>
+      <AuthProvider>
+        <Stack>
+          {/* Tambahkan ini: Sembunyikan header untuk halaman index (Splash) */}
+          <Stack.Screen name="index" options={{ headerShown: false }} />
+          
+          <Stack.Screen name="login" options={{ headerShown: false }} />
+          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+          <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
+        </Stack>
+      </AuthProvider>
       <StatusBar style="auto" />
     </ThemeProvider>
   );
